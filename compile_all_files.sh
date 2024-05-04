0f6b664a165a989ead90a32487db17b78ead714e
Abhishek Jagushte <abhishekjagushte@gmail.com>
2024-05-04 14:56:50 +0530

    compile 50 files ata a time

--- compile_all_files.sh
+++ compile_all_files.sh
@@ -1,5 +1,5 @@
 # Compile and execute all C++ programs in the current directory
-find . -maxdepth 1 -type f -name "*.cpp" | xargs -n 1 -P 1 -I {} sh -c '
+find . -maxdepth 1 -type f -name "*.cpp" | xargs -n 1 -P 50 -I {} sh -c '
     file="$1"
     output="${file%.cpp}.out"
     output_txt="${file%.cpp}_output.txt"
