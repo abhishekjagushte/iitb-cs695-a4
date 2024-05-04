9f5601d0bcdedf1b7bebcd327ac366a955b5933f
Abhishek Jagushte <abhishekjagushte@gmail.com>
2024-05-04 14:54:20 +0530

    added script to compile all cpp files in the custom workload

---
+++ compile_all_files.sh
@@ -0,0 +1,8 @@
+# Compile and execute all C++ programs in the current directory
+find . -maxdepth 1 -type f -name "*.cpp" | xargs -n 1 -P 1 -I {} sh -c '
+    file="$1"
+    output="${file%.cpp}.out"
+    output_txt="${file%.cpp}_output.txt"
+    echo "Compiling $file to $output"
+    g++ "$file" -o "$output"
+' _ {}
